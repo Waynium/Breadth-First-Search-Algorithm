d69af84ac29f0c44d53c412f29a10705b1bbf056
Wandile Nyembe <Wandile.Nyembe@bbd.co.za>
2025-08-19 23:06:29 +0200

    fix: fixed gitignore, cleaned up components to suit newer version of react

--- src/PathFindingVisualizer/Node/Node.jsx
+++ src/PathFindingVisualizer/Node/Node.jsx
@@ -2,39 +2,37 @@ import React, { Component } from 'react';
 
 import './Node.css';
 
-export default class Node extends Component {
-
-    render() { 
-        const {
-            col, 
-            isFinish, 
-            isStart, 
-            isWall, 
-            onMouseDown, 
-            onMouseEnter, 
-            onMouseUp, 
-            row
-        } = this.props;
+const Node = ({
+    col, 
+    isFinish, 
+    isStart, 
+    isWall, 
+    onMouseDown, 
+    onMouseEnter, 
+    onMouseUp, 
+    row,
+}) => {
         
-        const extraClassName = 
-            isFinish 
-            ? 'node-finish' 
-            : isStart 
-            ? 'node-start' 
-            : isWall
-            ? 'node-wall'
-            : '';
+    const extraClassName = 
+        isFinish 
+        ? 'node-finish' 
+        : isStart 
+        ? 'node-start' 
+        : isWall
+        ? 'node-wall'
+        : '';
 
-        return (
-            <>
-                <div 
-                    id={`node-${row}-${col}`} // This helps to identify which nodes are in the shortest path
-                    className={`node ${extraClassName}`}
-                    onMouseDown={() => onMouseDown(row, col)}
-                    onMouseEnter={() => onMouseEnter(row, col)}
-                    onMouseUp={() => onMouseUp(row, col)}>
-                </div>
-            </>
-        );
-    }
+    return (
+        <>
+            <div 
+                id={`node-${row}-${col}`} // This helps to identify which nodes are in the shortest path
+                className={`node ${extraClassName}`}
+                onMouseDown={() => onMouseDown(row, col)}
+                onMouseEnter={() => onMouseEnter(row, col)}
+                onMouseUp={() => onMouseUp(row, col)}>
+            </div>
+        </>
+    );
 }
+
+export default Node;
--- src/PathFindingVisualizer/PathFindingVisualizer.jsx
+++ src/PathFindingVisualizer/PathFindingVisualizer.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Node from './Node/Node';
 import { Dijkstra, getNodesInShortestPathOrder, getAllNodes } from '../Algorithms/Dijkstra';
  
@@ -9,213 +9,13 @@ const START_NODE_COL = 4;
 const FINISH_NODE_ROW = 10;
 const FINISH_NODE_COL = 45;
 
-export default class PathFindingVisualizer extends Component {
-    constructor(props) {
-        super(props); 
-        this.state = {
-            grid: [],
-            mouseIsPressed: false,
-        };
-    }
-
-    componentDidMount = () => {
-        const grid = getInitialGrid();
-        this.setState({grid});
-    }
-    
-    /**
-     * Method used to generate wall when mouse is pressed
-     */
-    handleMouseDown = (row, col) => {
-        const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
-        this.setState({grid: newGrid, mouseIsPressed: true});
-    }
-
-    handleMouseEnter = (row, col) => {
-        if (!this.state.mouseIsPressed) return;
-        const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
-        this.setState({grid: newGrid});
-    }
-
-    handleMouseUp = () => {
-        this.setState({mouseIsPressed: false});
-    }
-
-    /**
-     * @param visitedNodesInOrder => an array of visted nodes in order
-     * @param nodesInShortestPathOrder 
-     * @returns 
-     */
-    animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder) {
-        for (let i = 0; i <= visitedNodesInOrder.length; i++) {
-            if (i === visitedNodesInOrder.length) {
-                // set timeout method is a function that's used to execute an     
-                // instruction for a certain amount of time, which is provided     
-                // as one of the parameters 
-                setTimeout(() => {
-                  this.animateShortestPath(nodesInShortestPathOrder);
-                }, 20 * i);
-                return;
-            }
-            setTimeout(() => {
-                const node = visitedNodesInOrder[i];
-                if (node.isStart) 
-                    document.getElementById(`node-${node.row}-${node.col}`).className = 'node node-start';
-                else if (node.isFinish) 
-                    document.getElementById(`node-${node.row}-${node.col}`).className = 'node node-finish';
-                else 
-                    document.getElementById(`node-${node.row}-${node.col}`).className = 'node node-visited';
-            }, 20 * i);
-        }
-    }
-    
-    /**
-     * This method is for literally adding colour, or drawing the nodes 
-     * that are within the shortest path to the finish node
-     */
-    animateShortestPath(nodesInShortestPathOrder) {
-        for (let i=0; i<nodesInShortestPathOrder.length; i++) {
-            setTimeout(() => {
-                // As we see, the node is defined according to the index number of the array 
-                // which carries the nodes that are in the shortest path order      
-                // So that they can be specifically reffered to the CSS element, that animates them
-                const node = nodesInShortestPathOrder[i];
-                //if (node.row !== START_NODE_ROW && node.col !== START_NODE_COL) {
-                document.getElementById(`node-${node.row}-${node.col}`).className = 'node node-shortest-path';
-                //}
-
-            }, 50 * i);
-        }
-    }
-
-    /**
-     * This function is for making the process of dijkstra's algorithm visual,
-     * using the grid as the nodes
-     */
-    visualizeDijkstra() {
-        const {grid} = this.state;
-        const startNode = grid[START_NODE_ROW][START_NODE_COL];
-        const finishNode = grid[FINISH_NODE_ROW][FINISH_NODE_COL];
-        // the dijstra method returns the visited nodes in order,
-        const visitedNodesInOrder = Dijkstra(grid, startNode, finishNode);
-        const nodesInShortestPathOrder = getNodesInShortestPathOrder(finishNode);
-        this.animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder);
-    }
-
-    cleanVisitedNodes() {
-        const {grid} = this.state;
-        const startNode = grid[START_NODE_ROW][START_NODE_COL];
-        const finishNode = grid[FINISH_NODE_ROW][FINISH_NODE_COL];
-        // the dijstra method returns the visited nodes in order,
-        const visitedNodesInOrder = Dijkstra(grid, startNode, finishNode);
-        const nodesInShortestPathOrder = getNodesInShortestPathOrder(finishNode);
-        for (let i = 0; i <= visitedNodesInOrder.length; i++) {
-            if (i === visitedNodesInOrder.length) {
-                return;
-            }
-            setTimeout(() => {
-                const node = visitedNodesInOrder[i];
-                if (node.isStart) 
-                    document.getElementById(`node-${node.row}-${node.col}`).className = 'node node-start';
-                else if (node.isFinish) 
-                    document.getElementById(`node-${node.row}-${node.col}`).className = 'node node-finish';
-                else 
-                    document.getElementById(`node-${node.row}-${node.col}`).className = 'node';
-            }, 20 * i);
-        }
-    }
-    /*
-    generateWall() {
-        const {grid} = this.state;
-        const startNode = grid[START_NODE_ROW][START_NODE_COL];
-        const finishNode = grid[FINISH_NODE_ROW][FINISH_NODE_COL];
-        // the dijstra method returns the visited nodes in order,
-        const allNodes = getAllNodes(grid);
-        var x = 0;
-        var y = 0;
-        //let x = Math.floor((Math.random() * 10) + 1);
-        for (let i = 0; i <= 20; i++) {
-            if (i === allNodes.length) {
-                return;
-            }
-            // eslint-disable-next-line no-loop-func
-            setTimeout(() => {
-                const node = allNodes[i];
-                if (node.isStart) {
-                    document.getElementById(`node-${node.row}-${node.col}`).className = 'node node-start';
-                }
-                else if (node.isFinish) {
-                    document.getElementById(`node-${node.row}-${node.col}`).className = 'node node-finish';
-                }
-                else {
-                    x = Math.floor((Math.random() * 25) + 10); // columns
-                    y = Math.floor((Math.random() * 25) + 3); // rows
-                    document.getElementById(`node-${y}-${x}`).className = 'node node-wall';
-                }
-            }, 20 * i);
-        }
-
-    }*/
-
-    render() {
-        const {grid, mouseIsPressed} = this.state;
-
-        return (
-            // This div returns the grid for the nodes
-            <>
-                <div className="header__container">
-                    <h2 className="header">Breadth First Search Algorithm</h2>
-                    <button className="btn" id="primary" onClick={() => this.visualizeDijkstra()}>
-                        Search for Red Node
-                    </button>
-                    <button className="btn" id="primary" onClick={() => this.cleanVisitedNodes()}>
-                        Clean visited Nodes
-                    </button>
-                    {/*<button className="btn" onClick={() => this.generateWall()}>
-                        Generate New Wall
-                    </button>*/}
-                    <div className="header__container"></div>
-                </div>
-                <div className="grid"> {/**This is where the whole grid is rendered */}
-                    {grid.map((row, rowIdx) => {
-                        return (
-                            <div key={rowIdx}>
-                                {row.map((node, nodeIdx) => {
-                                    // destructuring the node object into the respective variables
-                                    const {row, col, isStart, isFinish, isWall} = node;
-                                    return (
-                                    <Node 
-                                        key={nodeIdx} 
-                                        col={col} 
-                                        isFinish={isFinish}
-                                        isStart={isStart}
-                                        isWall={isWall}
-                                        mouseIsPressed={mouseIsPressed}
-                                        onMouseDown={(row, col) => this.handleMouseDown(row, col)}
-                                        onMouseEnter={(row, col) => this.handleMouseEnter(row, col)}
-                                        onMouseUp={() => this.handleMouseUp()}
-                                        row={row}>
-                                    </Node>
-                                    );
-                                })}
-                            </div>
-                        );
-                    })}
-                </div>
-                <div className="copyright">
-                    <small>&copy; Copyright Wandile Nyembe 2022 - All rights reserved.</small>
-                </div>
-            </>
-        );
-    }
-}
-
 /**
  * This function is for creating the nodes, using 2 dimensional arrays
  * Simply rows and columns
  * @returns a grid
  */
 const getInitialGrid = () => {
+    
     const grid = [];
     for (let row=0; row<23; row++) {
         const currentRow = []; // an array of nodes
@@ -251,10 +51,236 @@ const getNewGridWithWallToggled = (grid, row, col) => {
     console.log("getNewGridWithWallToggled method")
     console.log("row: " + row + " col: " + col);
     const node = newGrid[row][col];
+
     const newNode = {
         ...node,
         isWall: !node.isWall,
     };
+
     newGrid[row][col] = newNode;
     return newGrid;
 };
+
+const PathFindingVisualizer = props => {
+
+    const [
+        state,
+        setState,
+    ] = React.useState({
+        grid: [],
+        mouseIsPressed: false,
+    })
+
+    /**
+     * Method used to generate wall when mouse is pressed
+     */
+    const handleMouseDown = (row, col) => {
+
+        const newGrid = getNewGridWithWallToggled(state.grid, row, col);
+        setState({
+            grid: newGrid, 
+            mouseIsPressed: true,
+        });
+    }
+
+    const handleMouseEnter = (row, col) => {
+
+        if (!state.mouseIsPressed) return;
+
+        const newGrid = getNewGridWithWallToggled(state.grid, row, col);
+        setState(prevState => ({
+            ...prevState,
+            grid: newGrid,
+        }));
+    }
+
+    const handleMouseUp = () => {
+
+        setState(prevState => ({
+            ...prevState,
+            mouseIsPressed: false,
+        }));
+    }
+
+    /**
+     * @param visitedNodesInOrder => an array of visted nodes in order
+     * @param nodesInShortestPathOrder 
+     * @returns 
+     */
+    const animateDijkstra = (visitedNodesInOrder, nodesInShortestPathOrder) => {
+
+        for (let i = 0; i <= visitedNodesInOrder.length; i++) {
+            if (i === visitedNodesInOrder.length) {
+                // set timeout method is a function that's used to execute an     
+                // instruction for a certain amount of time, which is provided     
+                // as one of the parameters 
+                setTimeout(() => {
+                  animateShortestPath(nodesInShortestPathOrder);
+                }, 20 * i);
+                return;
+            }
+            setTimeout(() => {
+                const node = visitedNodesInOrder[i];
+                if (node.isStart) 
+                    document.getElementById(`node-${node.row}-${node.col}`).className = 'node node-start';
+                else if (node.isFinish) 
+                    document.getElementById(`node-${node.row}-${node.col}`).className = 'node node-finish';
+                else 
+                    document.getElementById(`node-${node.row}-${node.col}`).className = 'node node-visited';
+            }, 20 * i);
+        }
+    }
+
+    /**
+     * This method is for literally adding colour, or drawing the nodes 
+     * that are within the shortest path to the finish node
+     */
+    const animateShortestPath = (nodesInShortestPathOrder) => {
+        for (let i=0; i<nodesInShortestPathOrder.length; i++) {
+            setTimeout(() => {
+                // As we see, the node is defined according to the index number of the array 
+                // which carries the nodes that are in the shortest path order      
+                // So that they can be specifically reffered to the CSS element, that animates them
+                const node = nodesInShortestPathOrder[i];
+                //if (node.row !== START_NODE_ROW && node.col !== START_NODE_COL) {
+                document.getElementById(`node-${node.row}-${node.col}`).className = 'node node-shortest-path';
+                //}
+
+            }, 50 * i);
+        }
+    }
+
+    /**
+     * This function is for making the process of dijkstra's algorithm visual,
+     * using the grid as the nodes
+     */
+    const visualizeDijkstra = () => {
+        const { grid } = state;
+        const startNode = grid[START_NODE_ROW][START_NODE_COL];
+        const finishNode = grid[FINISH_NODE_ROW][FINISH_NODE_COL];
+        // the dijstra method returns the visited nodes in order,
+        const visitedNodesInOrder = Dijkstra(grid, startNode, finishNode);
+        const nodesInShortestPathOrder = getNodesInShortestPathOrder(finishNode);
+        animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder);
+    }
+
+    const cleanVisitedNodes = () => {
+        const { grid } = state;
+        const startNode = grid[START_NODE_ROW][START_NODE_COL];
+        const finishNode = grid[FINISH_NODE_ROW][FINISH_NODE_COL];
+        // the dijstra method returns the visited nodes in order,
+        const visitedNodesInOrder = Dijkstra(grid, startNode, finishNode);
+        const nodesInShortestPathOrder = getNodesInShortestPathOrder(finishNode);
+        for (let i = 0; i <= visitedNodesInOrder.length; i++) {
+            if (i === visitedNodesInOrder.length) {
+                return;
+            }
+            setTimeout(() => {
+                const node = visitedNodesInOrder[i];
+                if (node.isStart) 
+                    document.getElementById(`node-${node.row}-${node.col}`).className = 'node node-start';
+                else if (node.isFinish) 
+                    document.getElementById(`node-${node.row}-${node.col}`).className = 'node node-finish';
+                else 
+                    document.getElementById(`node-${node.row}-${node.col}`).className = 'node';
+            }, 20 * i);
+        }
+    }
+
+    /*
+    generateWall() {
+        const {grid} = this.state;
+        const startNode = grid[START_NODE_ROW][START_NODE_COL];
+        const finishNode = grid[FINISH_NODE_ROW][FINISH_NODE_COL];
+        // the dijstra method returns the visited nodes in order,
+        const allNodes = getAllNodes(grid);
+        var x = 0;
+        var y = 0;
+        //let x = Math.floor((Math.random() * 10) + 1);
+        for (let i = 0; i <= 20; i++) {
+            if (i === allNodes.length) {
+                return;
+            }
+            // eslint-disable-next-line no-loop-func
+            setTimeout(() => {
+                const node = allNodes[i];
+                if (node.isStart) {
+                    document.getElementById(`node-${node.row}-${node.col}`).className = 'node node-start';
+                }
+                else if (node.isFinish) {
+                    document.getElementById(`node-${node.row}-${node.col}`).className = 'node node-finish';
+                }
+                else {
+                    x = Math.floor((Math.random() * 25) + 10); // columns
+                    y = Math.floor((Math.random() * 25) + 3); // rows
+                    document.getElementById(`node-${y}-${x}`).className = 'node node-wall';
+                }
+            }, 20 * i);
+        }
+
+    }*/
+
+    React.useEffect(() => {
+        
+        const newGrid = getInitialGrid();
+        setState(prevState => ({
+            ...prevState,
+            grid: newGrid,
+        }));
+    },[])
+
+    console.log('checking re-renders');
+
+    return (
+        // This div returns the grid for the nodes
+        <React.Fragment>
+
+            <div className="header__container">
+                <h2 className="header">Breadth First Search Algorithm</h2>
+                <button className="btn" id="primary" onClick={() => visualizeDijkstra()}>
+                    Search for Red Node
+                </button>
+                <button className="btn" id="primary" onClick={() => cleanVisitedNodes()}>
+                    Clean visited Nodes
+                </button>
+                {/*<button className="btn" onClick={() => this.generateWall()}>
+                    Generate New Wall
+                </button>*/}
+                <div className="header__container"></div>
+            </div>
+            <div className="grid"> {/**This is where the whole grid is rendered */}
+
+                { state.grid.map((row, rowIdx) => {
+                    return (
+                        <div key={rowIdx}>
+                            {row.map((node, nodeIdx) => {
+                                // destructuring the node object into the respective variables
+                                const {row, col, isStart, isFinish, isWall} = node;
+                                return (
+                                    <Node 
+                                        key={nodeIdx} 
+                                        col={col} 
+                                        isFinish={isFinish}
+                                        isStart={isStart}
+                                        isWall={isWall}
+                                        mouseIsPressed={state.mouseIsPressed}
+                                        onMouseDown={(row, col) => handleMouseDown(row, col)}
+                                        onMouseEnter={(row, col) => handleMouseEnter(row, col)}
+                                        onMouseUp={() => handleMouseUp()}
+                                        row={row}>
+                                    </Node>
+                                );
+                            })}
+                        </div>
+                    );
+                })}
+
+            </div>
+            <div className="copyright">
+                <small>&copy; Copyright Wandile Nyembe 2022 - All rights reserved.</small>
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default PathFindingVisualizer;
