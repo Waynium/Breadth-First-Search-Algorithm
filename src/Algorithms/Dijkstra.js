572ea3e3a0979be773ae1e770bf6737217cb3441
Wandile Nyembe <Wandile.Nyembe@bbd.co.za>
2025-08-19 23:01:39 +0200

    fix: added gitignore to ignore node_modules and build file

--- src/Algorithms/Dijkstra.js
+++ src/Algorithms/Dijkstra.js
@@ -88,14 +88,19 @@ function updateUnvisitedNeighbours(node, grid) {
  * It then returns a list of the 4 neighbours
  */
 function getUnvisitedNeighbours (node, grid) {
+
     const neighbours = [];
     const {col, row} = node;
+
     if (row > 0) neighbours.push(grid[row - 1][col]); 
     if (row < grid.length - 1) neighbours.push(grid[row + 1][col]); 
+
     console.log("getUnvisitedNeighbours method"); // debugging lines
     console.log("row: " + row + " col: " + col);
+
     if (col > 0) neighbours.push(grid[row][col - 1]); 
     if (col < grid[0].length - 1) neighbours.push(grid[row][col + 1]); 
+    
     return neighbours.filter(neighbour => !neighbour.isVisited);
 }
 
